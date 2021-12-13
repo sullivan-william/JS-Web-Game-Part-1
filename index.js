9aa85251cf96c3a4cbace3f17d4c2d187c2088eb
William Sullivan <sullivanwp95@gmail.com>
2021-12-13 16:38:01 -0500

    Added bonus and modified z index so background would not block other images

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function newImage(image, leftPosition, bottomPosition) {
+function newImage(image, leftPosition, bottomPosition, z) {
     let object = document.createElement('img')
     object.src = image
     object.style.position = 'fixed'
     object.style.left = leftPosition + 'px'
     object.style.bottom = bottomPosition + 'px'
+    object.style.zIndex = z
     document.body.append(object)
     return object
 }
 
-function newItem(image, leftPosition, bottomPosition) {
-    let object = newImage(image, leftPosition, bottomPosition)
+function newItem(image, leftPosition, bottomPosition, z) {
+    let object = newImage(image, leftPosition, bottomPosition, z)
 
     object.addEventListener('dblclick', function() {
         object.remove()
     })
 }
 
-newImage('assets/green-character.gif', 100, 100)
+newImage('assets/green-character.gif', 100, 100, 1)
 // let greenCharacter = document.createElement('img')
 // greenCharacter.src = 'assets/green-character.gif'
 // greenCharacter.style.position = 'fixed'
@@ -24,7 +25,7 @@ newImage('assets/green-character.gif', 100, 100)
 // greenCharacter.style.bottom = '100px'
 // document.body.append(greenCharacter)
 
-newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/pine-tree.png', 450, 200, 1)
 // let pineTree = document.createElement('img')
 // pineTree.src = 'assets/pine-tree.png'
 // pineTree.style.position = 'fixed'
@@ -32,13 +33,28 @@ newImage('assets/pine-tree.png', 450, 200)
 // pineTree.style.bottom = '200px'
 // document.body.append(pineTree)
 
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
+newImage('assets/tree.png', 200, 300, 1)
+newImage('assets/pillar.png', 350, 100, 1)
+newImage('assets/crate.png', 150, 200, 1)
+newImage('assets/well.png', 500, 425, 1)
 
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+newItem('assets/sword.png', 500, 405, 1)
+newItem('assets/shield.png', 165, 185, 1)
+newItem('assets/staff.png', 600, 100, 1)
 
 // bonus
+
+function tile(image, leftPosition, bottomPosition, z, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(image, leftPosition + w*100, bottomPosition + h*100, z)
+        }
+    }
+}
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, 0, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, 0, window.innerWidth/100, heightOfGrass/100)
